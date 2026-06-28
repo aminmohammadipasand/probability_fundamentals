8243b907fb4744eef3d609cee8b731f39db76cb5
aminmohammadipasand <aminmohammadipasand@gmail.com>
2026-06-28 15:19:01 +0330

    phase 1 completed

--- main.py
+++ main.py
@@ -33,3 +33,46 @@ class PoissonDistribution:
             p *= u
             
         return k - 1
+
+class ExponentialDistribution:
+    def __init__(self, lam, random_engine):
+        self.lam = lam
+        self.engine = random_engine
+
+    def generate_sample(self):
+        u = self.engine.next_random()       
+        while u == 0:
+            u = self.engine.next_float()
+        
+        return -math.log(u) / self.lam
+
+import math
+
+class NormalDistribution:
+    def __init__(self, mu, sigma, random_engine):
+        self.mu = mu
+        self.sigma = sigma
+        self.engine = random_engine
+        self._next_gauge = None
+
+    def generate_sample(self):
+        if self._next_gauge is not None:
+            z = self._next_gauge
+            self._next_gauge = None
+            return self.mu + z * self.sigma
+
+        u1 = self.engine.next_float()
+        u2 = self.engine.next_float()
+
+        while u1 == 0:
+            u1 = self.engine.next_float()
+
+        r = math.sqrt(-2.0 * math.log(u1))
+        theta = 2.0 * math.pi * u2
+
+        z0 = r * math.cos(theta)
+        z1 = r * math.sin(theta)
+
+        self._next_gauge = z1
+
+        return self.mu + z0 * self.sigma
