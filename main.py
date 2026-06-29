5db18d54c16f6e351e6826370acecabc32a18fd3
aminmohammadipasand <aminmohammadipasand@gmail.com>
2026-06-29 13:57:34 +0330

    complete limit theorem part in phase 3

--- main.py
+++ main.py
@@ -46,8 +46,6 @@ class ExponentialDistribution:
         
         return -math.log(u) / self.lam
 
-import math
-
 class NormalDistribution:
     def __init__(self, mu, sigma, random_engine):
         self.mu = mu
@@ -76,3 +74,87 @@ class NormalDistribution:
         self._next_gauge = z1
 
         return self.mu + z0 * self.sigma
+
+class LimitTheoremsEvaluator:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def normal_pdf(x):
+        return (1.0 / math.sqrt(2 * math.pi)) * math.exp(-0.5 * (x ** 2))
+
+    @staticmethod
+    def normal_cdf_standard(z):
+        lower_bound = -10.0
+        if z < lower_bound:
+            return 0.0
+        if z > 10.0:
+            return 1.0
+        
+        steps = 10000
+        h = (z - lower_bound) / steps
+        
+        area = 0.5 * (LimitTheoremsEvaluator.normal_pdf(lower_bound)  + LimitTheoremsEvaluator.normal_pdf(z))
+        for i in range(1, steps):
+            x = lower_bound + i * h
+            area += LimitTheoremsEvaluator.normal_pdf(x)
+        
+        return area * h
+
+    def get_normal_probability(self, a, b, mu, sigma):
+        z_a = (a - mu) / sigma
+        z_b = (b - mu) / sigma
+        
+        return self.normal_cdf_standard(z_b) - self.normal_cdf_standard(z_a)
+
+    def evaluate_binomial_to_normal(self, binomial_samples, n, p, a, b):
+        M = len(binomial_samples)
+        
+        success_count = sum(1 for x in binomial_samples if a <= x <= b)
+        empirical_prob = success_count / M
+        
+        mu = n * p
+        sigma = math.sqrt(n * p * (1 - p))
+        
+        a_corrected = a - 0.5
+        b_corrected = b + 0.5
+        
+        theoretical_prob = self.get_normal_probability(a_corrected, b_corrected, mu, sigma)
+        
+        error_pct = (abs(empirical_prob - theoretical_prob) / theoretical_prob) * 100
+        
+        return empirical_prob, theoretical_prob, error_pct
+
+    def evaluate_poisson_to_normal(self, poisson_samples, lam, a, b):
+        """سنجش تقریب پوآسون به نرمال با اعمال تصحیح پیوستگی"""
+        M = len(poisson_samples)
+        
+        success_count = sum(1 for x in poisson_samples if a <= x <= b)
+        empirical_prob = success_count / M
+        
+        mu = lam
+        sigma = math.sqrt(lam)
+        
+        a_corrected = a - 0.5
+        b_corrected = b + 0.5
+        
+        theoretical_prob = self.get_normal_probability(a_corrected, b_corrected, mu, sigma)
+        
+        error_pct = (abs(empirical_prob - theoretical_prob) / theoretical_prob) * 100
+        
+        return empirical_prob, theoretical_prob, error_pct
+
+
+if __name__ == "__main__":
+    engine = XorShiftEngine()
+    evaluator = LimitTheoremsEvaluator()
+    pois = PoissonDistribution(40, engine)
+    print("\n Piosson to normal error computing:")
+    poisson_data = [pois.generate_sample() for _ in range(10 ** 4)]
+    lam = 40
+    a_poi, b_poi = 30, 40
+    emp_p_poi, theo_p_poi, err_poi = evaluator.evaluate_poisson_to_normal(poisson_data, lam, a_poi, b_poi)
+    print(f"range: [{a_poi}, {b_poi}]")
+    print(f"empirical probability: {emp_p_poi:.5f}")
+    print(f"theoretical pobability: {theo_p_poi:.5f}")
+    print(f"error precetage: {err_poi:.2f}%")
